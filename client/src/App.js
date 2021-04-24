da405a5ee2caeb426ef5c70c28f3f0579add3ce4
Staci Genesi <sgenesi@hotmail.com>
2021-04-24 13:52:57 -0600

    new project

---
+++ client/src/App.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./components/Item";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+import Footer from './components/Footer';
+import { v4 as uuidv4 } from "uuid";
+
+import './App.css';
+
+const arr = () => {
+  let data = localStorage.getItem("data");
+  if (data) return JSON.parse(localStorage.getItem("data"));
+  else return [];
+};
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+function App() {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState(arr);
+
+  const handleSubmit = (e) => {
+    const newItem = {
+      id: uuidv4(),
+      item: item,
+      complete: false,
+    };
+    e.preventDefault();
+    if (item && item.length <= 25) {
+      setList([...list, newItem]);
+      setItem("");
+    }
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(list));
+  }, [list]);
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  };
+
+  return (
+    <div className="App">
+
+      <h1>Grocery List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          value={item}
+          placeholder="Enter the items"
+          onChange={handleChange}
+        />
+        <button className="btn" type="submit">
+          Add Items
+        </button>
+        <br></br>
+        <br></br>
+      </form>
+      <div>
+        {list.map((c, id) => (
+          <Item
+            key={id}
+            id={c.id}
+            item={c.item}
+            list={list}
+            setList={setList}
+            complete={c.complete}
+            setItem={setItem}
+          />
+        ))}
+      </div>
+      <Footer></Footer>
+    </div >
+  );
+}
+
+export default App;
+
---
+++ client/src/components/Item/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Item.css";
+
+const Item = ({ id, item, list, setList, complete }) => {
+    const remove = (id) => {
+        setList(list.filter((el) => el.id !== id));
+    };
+
+    const handleComplete = (id) => {
+        setList(
+            list.map((item) => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        complete: !item.complete,
+                    };
+                }
+                return item;
+            })
+        );
+    };
+
+    const handleItem = (e) => {
+        if (e.target.value.length <= 25) {
+            setList(
+                list.map((el) => {
+                    if (el.id === id) {
+                        return {
+                            ...el,
+
+                            item: e.target.value,
+                        };
+                    }
+
+                    return el;
+                })
+            );
+        }
+    };
+
+    return (
+        <div className="item">
+            <input
+                type="text"
+                value={item}
+                style={{
+                    border: "none",
+                    outline: "none",
+                    backgroundColor: "transparent",
+                    color: "black",
+                    fontSize: "20px",
+                }}
+                onChange={handleItem}
+                className={complete ? "complete" : ""}
+            />
+            <img
+                onClick={() => handleComplete(id)}
+                src="https://img.icons8.com/metro/26/000000/checkmark.png"
+            />
+            <img
+                onClick={() => remove(id)}
+                src="https://img.icons8.com/metro/26/000000/delete-sign.png"
+                alt="Delete"
+            />
+        </div>
+    );
+};
+export default Item;
